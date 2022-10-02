34f4dba678daaf9faf1cbe2b864388341cc79a35
Liza Martyshina <martyshinaliza@gmail.com>
2022-10-02 19:01:58 +0300

    Remove

--- main.py
+++ main.py
@@ -2,13 +2,6 @@
 def print_hi(name):
     print(f'Hi, {name}')
 
-def foo():
-    print('Foo!')
-
-def bar():
-    print('Bar!')
-
 if __name__ == '__main__':
     print_hi('PyCharm')
-    foo()
     bar()
